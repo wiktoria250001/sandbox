d178ad0f6c6e5c72e5fd1d15113416dbcf1321f2
wiktoria250001 <149382337+wiktoria250001@users.noreply.github.com>
2023-11-06 14:49:17 +0100

    chore add gui

--- main.py
+++ main.py
@@ -1,62 +1,15 @@
 from newone import users_list
-
-def add_user_to(users_list:list) -> None:
-    """
-    add object to list
-    :param users_list: user list
-    :return: None
-    """
-    name = input('podaj imie?')
-    posts = input('podaj liczbe postów?')
-    users_list.append({"name": name,"posts": posts})
-name = input(' podaj imie uzytkownika do usunecia: ')
-
-def remove_user_from(users_list: list) -> None:
-    """
-    remove object from list
-    :param users_list: List - user list
-    :return: None
-    """
-    tmp_list = []
-    name = input('podaj imie uzytkownika do usuniecia:')
-
-    for user in users_list:
-        if user["name"]== name:
-            print(f'Znaleziono uzytkownika{user}')
-            #users_list.remove(user)
-            tmp_list.append(user)
-    print('Znaleziono nastepujacych uzytkownikow :')
-    print("0: Usun wszyskich znalezionych uzytkownikow")
-    for numerek,user_to_be_removed in enumerate(tmp_list):
-        print( f'{numerek+1}: {user_to_be_removed}')
-    numer = int(input(f'wybierz numer uzytkownka do usuniecia: '))
-    if numer == 0:
-        for user in tmp_list:
-            if user['name'] == name:
-                users_list.remove(tmp_list[numer-1])
-    else:
-     users_list.remove(tmp_list[numer-1])
-
-    # print(numer)
-    # print(tmp_list[numer-1])
-    #   users_list.remove(tmp_list[numer-1])
-
-
-     remove_user_from(users_list)
-
-
+from utils.my_functions import add_user_to, remove_user_from, show_users_from
 
 #add_user_to(users_list)
-#posts = input('podaj liczbe postów?')
-#users_list.append({'name': name,'posts': posts})
-#name = input('podaj imie?')
-#posts = input('podaj liczbe postów?')
-#users_list.append({'name': name,'posts': posts})
-#make it work, make it properly, make it fast
+# remove_user_from(users_list)
+# show_users_from(users_list)
 
-for user in users_list:
-    #print(f'}users_list)
-    print(f'Twój znajomy {user['name']} dodał {user["posts"]} ')
+
+gui()
+#for user in users_list:
+    #print(f'users_list)
+    #print(f'Twój znajomy {user['name']} dodał {user["posts"]} ')
 
 
 
