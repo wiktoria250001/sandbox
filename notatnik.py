c50aa0fbb9a29559109527069618135550635cc3
wiktoria250001 <149382337+wiktoria250001@users.noreply.github.com>
2023-12-18 22:55:03 +0100

    Update notatnik.py

--- notatnik.py
+++ notatnik.py
@@ -1,18 +1,61 @@
-import requests
-    class User:
-        def __init__(self, miasto): -> object:
-            self.miasto = miasto
+import random
 
-        def pogoda_z(self, miasto: str):
-            url = f"https://danepubliczne.imgw.pl/api/data/synop/station/{miasto}"
-            return requests.get(url).json()
+import sqlalchemy
+import os
+import sqlalchemy.orm
+from dotenv import load_dotenv
+from geoalchemy2 import Geometry
+from faker import Faker
 
-    npc_1=User(miasto="warszawa") #linijki do tworzenia nazwy
-    npc_2=User(miasto="zamosc")
-    print(npc_1.miasto)
-    print(npc_2.miasto)
+load_dotenv()
 
-    print(npc_1.pogoda_z(npc_1.miasto))
-    print(npc_2.pogoda_z(npc_2.miasto))
+db_params = sqlalchemy.URL.create(
+    drivername='postgresql+psycopg2',
+    username=os.getenv("POSTGRES_USER"),
+    password=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
+    database=os.getenv("POSTGRES_DB"),
+    port=os.getenv("POSTGRES_PORT"),
 
-    pogoda_z("warszawa")
+
+)
+#print(type(os.getenv("POSGRES_USER")))
+engine = sqlalchemy.create_engine(db_params)
+connection= engine.connect()
+
+Base= sqlalchemy.orm.declarative_base()
+class User(Base):
+    __tablename__ = 'serafin_psip'
+    id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True)  # serial
+    name = sqlalchemy.Column(sqlalchemy.String(100), nullable=True)
+    location = sqlalchemy.Column('geom', Geometry(geometry_type='POINT', srid=4326), nullable=True)
+Base.metadata.create_all(engine)
+Session = sqlalchemy.orm.sessionmaker(bind=engine)
+session = Session()
+
+### Create / insert
+# lista_userow: list = []
+# fake = Faker()
+#
+# for item in range(10_000):
+#     lista_userow.append(
+#         User(
+#             name=fake.name(),
+#             location=f'POINT({random.uniform(14,24)} {random.uniform(49,55)})'
+#         )
+#     )
+#
+# session.add_all(lista_userow)
+# session.commit()
+
+### Read / Select
+
+users_from_db = session.query(User).all()
+
+session.commit()
+
+
+
+session.flush()
+connection.close()
+engine.dispose()
